8bb610de6038687362f3de7f74c355a5700a36fd
gnisj <gnisj@protonmail.com>
2017-10-26 00:06:27 +0200

    Changed plots

--- tempplotter.py
+++ tempplotter.py
@@ -12,43 +12,45 @@ df = pd.read_sql_query("SELECT * from temps", con, index_col=None, parse_dates=[
 
 con.close()
 
+# Remove first 3 rows
 df2 = df.ix[3:]
+df = df.ix[3:]
+
+# Re-index dfx with time column
+df.set_index('timestamp', inplace=True)
+
+n_days = 7
+now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+starttime = datetime.now() - timedelta(days=n_days)#'2017-10-20 17:00:00'
+stoptime = now#'2017-10-22 20:00:00'
+
+mask = (df.index > starttime) & (df.index <= stoptime)
+print(mask)
+
 x = df2['timestamp'].astype(dt.datetime)
 
-#print(x.tail(10))
-#print(df2['temp'].tail(100))
-
 # Create a plot with matplotlib and store .png
-fig, axes = plt.subplots(3, figsize=(8,8), sharex=False)
-fig.text(0.5, 0.01, 'Time', fontsize=14, ha='center')
+fig, axes = plt.subplots(2, figsize=(8,8), sharex=False)
+fig.text(0.5, 0.05, 'Time', fontsize=14, ha='center')
 fig.text(0.06, 0.5, 'Temperature (Celcius)', fontsize=14, va='center', rotation='vertical')
 
+# Rolling mean N
+n = 12000
 
-#axes[0].plot(df2['timestamp'], df2['temp'])
+axes[0].plot(df.index, df['temp']/1000, color='black', alpha=0.3, label='Loft temperature')
+axes[0].plot(df.index, df['temp'].rolling(center=True, window=n).mean()/1000, color='black', label='Moving average')
 
-axes[0].plot(x, df2['temp']/1000, color='black', label='Loft')
-axes[1].plot(x, pd.rolling_mean(df2['temp']/1000, 3000), color='black', label='Loft moving average N=3000')
-#axes[2].plot(x.tail(100), (df2['temp'].tail(100))/1000, color='black', label='Last N=1000')
-             
-#axes[0].axhline(df2['temp'].mean(), color='black', linestyle='--', label="Mean temp: %.1f" % (df2['temp'].mean()))
-#axes[1].bar(df2['temp'].values, df2['temp'].values)
-
-#print(df2['temp'].values)
-#axes[0].xaxis_date()
-#axes[0].xaxis.set_major_formatter(dates.DateFormatter('%H:%M\n%d.%m.%y'))
-
-
+   
+axes[1].plot(df.index[mask], df['temp'][mask].rolling(center=True, window=10).mean()/1000, color='black', label='Last %d days' % n_days)
+          
 for ax in axes:
 	ax.grid(True, which='Both')
 	ax.xaxis_date()
 	ax.xaxis.set_major_formatter(dates.DateFormatter('%H:%M\n%d.%m.%y'))
-	ax.legend(bbox_to_anchor=(.95, .25), loc=1, borderaxespad=0., prop={'size':10})
+	ax.legend(bbox_to_anchor=(0.97, .97), loc=1, borderaxespad=0., prop={'size':10})
 	ax.tick_params(labelsize=11)
 	#ax.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
 
-#df2.plot(ax=axes[0])
-#df2.plot()
 
-
-fig.savefig('/data/rhomicron.com/www/loft_temp.jpg')
+fig.savefig('loft_temp.jpg')
 
